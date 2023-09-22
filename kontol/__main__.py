ef1458927287deccac6f4090bd901ef29a1779e0
kymang <140633527+kymang@users.noreply.github.com>
2023-09-22 21:08:16 +0700

    Update __main__.py

--- kontol/__main__.py
+++ kontol/__main__.py
@@ -1,43 +1,37 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 #from config import *
+from pyrogram.types import Message
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from datetime import datetime
 
 api_id = 9128356
 api_hash = "cf219228092173e15047d3fb5a023cfc"
 bot_token = "6125694353:AAH8lZhap_unS6wvYbCnBfMmXKv9M_vv_zg"
 
-app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+bot = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
-
-
-@app.on_message(filters.command("start"))
-def start_command(client, message):
-    # Membuat tombol-tombol untuk inline keyboard
-    button1 = InlineKeyboardButton("Lihat Produk", callback_data="view_products")
-    button2 = InlineKeyboardButton("Keranjang Belanja", callback_data="view_cart")
-    button3 = InlineKeyboardButton("Selesai Belanja", callback_data="checkout")
-
-    # Membuat markup inline keyboard
-    inline_markup = InlineKeyboardMarkup(
-        [[button1], [button2], [button3]]
+@bot.on_message(filters.command(["start"]))
+async def start_(client: Client, message: Message):
+    await message.reply_text(
+        f"""<b>👋 Helo {message.from_user.first_name} \n
+💭 Welcome to Magic Project Bot.\n There will be interesting things here, just wait bro.</b>""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(text="Repository", url=f"https://github.com/Team-Pesulap/MagicProject"),
+                    InlineKeyboardButton(text="Support", url=f"https://t.me/PesulapTelegram"),
+                ],
+                [
+                    InlineKeyboardButton(text="Deploy", url=f"https://dashboard.heroku.com/new?template=https://github.com/Team-Pesulap/MagicProject"),
+                ],
+		[
+                     InlineKeyboardButton(text="Tutup", callback_data="cl_ad"),
+                  ],
+             ]
+        ),
+     disable_web_page_preview=True
     )
-
-    # Mengirim pesan dengan inline keyboard sebagai respons perintah /start
-    message.reply_text("Selamat datang di Telegram Shop. Silakan pilih tindakan di bawah ini:", reply_markup=inline_markup)
-
-
-
-@app.on_callback_query()
-def handle_callback(client, query):
-    if query.data == "view_products":
-        # Tanggapan untuk tombol "Lihat Produk"
-        client.answer_callback_query(query.id, text="Anda akan melihat produk.")
-    elif query.data == "view_cart":
-        # Tanggapan untuk tombol "Keranjang Belanja"
-        client.answer_callback_query(query.id, text="Anda akan melihat keranjang belanja.")
-    elif query.data == "checkout":
-        # Tanggapan untuk tombol "Selesai Belanja"
-        client.answer_callback_query(query.id, text="Anda akan menyelesaikan belanja.")
 
 if __name__ == "__main__":
     app.run()
